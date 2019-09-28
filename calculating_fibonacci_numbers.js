e35cc02f861b65b5a9ad5d66d67c06f062903d2c
Eric Li <liyangzhong@hotmail.com>
2019-09-28 16:16:40 +0800

    add solution to an online question

--- calculating_fibonacci_numbers.js
+++ calculating_fibonacci_numbers.js
@@ -3,7 +3,24 @@
 var fibonacci = function(n){
 	return n<2 ? n : fibonacci(n-1) +  fibonacci(n-2);
 };
-for(var i = 0;i <=35; i+=1){
-	console.log(i+':'+fibonacci(i));
+var arrayOfFib= [];
+for(var i = 0;i <=3; i+=1){
+    console.log(i+':'+fibonacci(i));
+    arrayOfFib.push(fibonacci(i));
 }
+
+console.log(arrayOfFib);
 //======================
+//针对下面链接中提出的问题的解答：
+//https://ask.csdn.net/questions/387801
+//以下是用Javascript 计算前20个斐波那契数的代码：
+
+var fibonacci = function(n){
+	return n<2 ? n : fibonacci(n-1) +  fibonacci(n-2);
+};
+var arrayOfFibonacciNumbers= [];
+for(var i = 0;i <=19; i+=1){
+    arrayOfFibonacciNumbers.push(fibonacci(i));
+}
+
+console.log(arrayOfFibonacciNumbers);
